4de92cffb38cea49e3005da4b458aa2720b8dc9f
nastya <cctvporsh@gmail.com>
2020-08-31 12:03:22 +0300

    removed database name string

--- src/main/java/com/teamvoy/order/app/repository/impl/OrderDaoImpl.java
+++ src/main/java/com/teamvoy/order/app/repository/impl/OrderDaoImpl.java
@@ -11,15 +11,19 @@ import org.influxdb.dto.Query;
 import org.influxdb.dto.QueryResult;
 import org.influxdb.impl.InfluxDBResultMapper;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.influxdb.InfluxDBProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderDaoImpl implements OrderDao {
-    private static final String DB_NAME = "test";
+    private static final int ORDER_EXPIRATION_TIME = 10;
     private final InfluxDB database;
+    private final InfluxDBProperties properties;
 
-    public OrderDaoImpl(@Qualifier("influxDB") InfluxDB database) {
+    public OrderDaoImpl(@Qualifier("influxDB") InfluxDB database,
+                        InfluxDBProperties properties) {
         this.database = database;
+        this.properties = properties;
     }
 
     @Override
@@ -41,7 +45,7 @@ public class OrderDaoImpl implements OrderDao {
     @Override
     public List<Order> getAll() {
         String selectQuery = "SELECT * FROM \"order\"";
-        QueryResult queryResult = database.query(new Query(selectQuery, DB_NAME));
+        QueryResult queryResult = database.query(new Query(selectQuery, properties.getDatabase()));
         InfluxDBResultMapper resultMapper = new InfluxDBResultMapper();
         return resultMapper.toPOJO(queryResult, Order.class);
     }
@@ -49,7 +53,7 @@ public class OrderDaoImpl implements OrderDao {
     @Override
     public Order getById(Long id) {
         String selectQuery = "SELECT * FROM \"order\" WHERE \"id\" = " + id;
-        QueryResult queryResult = database.query(new Query(selectQuery, DB_NAME));
+        QueryResult queryResult = database.query(new Query(selectQuery, properties.getDatabase()));
         InfluxDBResultMapper resultMapper = new InfluxDBResultMapper();
         List<Order> orders = resultMapper.toPOJO(queryResult, Order.class);
         return orders.stream().findFirst().get();
@@ -58,13 +62,13 @@ public class OrderDaoImpl implements OrderDao {
     @Override
     public void delete(Long id) {
         String deleteQuery = "DELETE FROM \"order\" WHERE \"id\" = " + id;
-        database.query(new Query(deleteQuery, DB_NAME));
+        database.query(new Query(deleteQuery, properties.getDatabase()));
     }
 
     @Override
     public boolean isNotValid(Order order) {
         LocalDateTime now = LocalDateTime.now();
-        return now.isAfter(order.getCreationTIme().plusMinutes(10));
+        return now.isAfter(order.getCreationTIme().plusMinutes(ORDER_EXPIRATION_TIME));
     }
 }
 
